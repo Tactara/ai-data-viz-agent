35b41077037ad9270cb120438b0972421c857ff5
0xPerrier <kunzmainali@hotmail.com>
2025-04-19 19:17:13 -0500

    code interpretation

--- ai_data_viz_agent.py
+++ ai_data_viz_agent.py
@@ -13,3 +13,31 @@ import base64
 from io import BytesIO
 from together import Together
 from e2b_code_interpreter import Sandbox
+
+warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
+
+pattern = re.compile(r"```python\n(.*?)\n```", re.DOTALL)
+
+def code_interpret(e2b_code_interpreter: Sandbox, code: str) -> Optional[List[Any]]:
+    with st.spinner('Executing code in E2B sandbox...'):
+        stdout_capture = io.StringIO()
+        stderr_capture = io.StringIO()
+
+        with contextlib.redirect_stdout(stdout_capture), contextlib.redirect_stderr(stderr_capture):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                exec = e2b_code_interpreter.run_code(code)
+
+        if stderr_capture.getvalue():
+            print("[Code Interpreter Warnings/Errors]", file=sys.stderr)
+            print(stderr_capture.getvalue(), file=sys.stderr)
+
+        if stdout_capture.getvalue():
+            print("[Code Interpreter Output]", file=sys.stdout)
+            print(stdout_capture.getvalue(), file=sys.stdout)
+
+        if exec.error:
+            print(f"[Code Interpreter ERROR] {exec.error}", file=sys.stderr)
+            return None
+        return exec.results
+        
